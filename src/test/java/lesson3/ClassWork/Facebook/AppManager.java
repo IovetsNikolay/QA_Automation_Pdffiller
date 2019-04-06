4ab751f649ffc88500dde9976ea5853b00e2622e
cowy123 <indicow@gmail.com>
2019-04-06 19:24:49 +0300

    Facebook Task is refactored

--- src/test/java/lesson3/ClassWork/Facebook/AppManager.java
+++
@@ -1,8 +0,0 @@
-package lesson3.ClassWork.Facebook;
-
-public class AppManager {
-
-    public class ApplicationManager {
-
-    }
-}
---
+++ src/test/java/lesson3/ClassWork/Facebook/appmanager/AppManager.java
@@ -0,0 +1,70 @@
+package lesson3.ClassWork.Facebook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.concurrent.TimeUnit;
+
+public class AppManager {
+    private String url = "https://www.facebook.com";
+    private By firstNameInput = By.xpath("//input[@name='firstname']");                      //List of the locators
+    private By lastNameInput = By.xpath("//input[@name='lastname']");
+    private By phoneNumberInput = By.xpath("//input[@name='reg_email__']");
+    private By passwodInput = By.xpath("//input[@name='reg_passwd__']");
+    private By birthdayDayDropdown = By.xpath("//select[@id='day']");
+    private By birthdayMounthDropdown = By.xpath("//select[@id='month']");
+    private By birthdayYearDropdown = By.xpath("//select[@id='year']");
+    private By sexManInput = By.xpath("//input[@name='sex'][@value='1']");
+    private By sexWomanInput = By.xpath("//input[@name='sex'][@value='2']");
+    private By submitButton = By.xpath("//button[@name='websubmit']");
+
+
+    WebDriver driver;
+
+    protected void init() {
+        System.setProperty("webdriver.chrome.driver", "C:/webDrivers/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        driver = new ChromeDriver(options);
+        //driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+        //loginPage = new LoginPageHelper(driver);                           //Transfer created driver object in LoginPageHelper class by constructor
+
+        driver.navigate().to(url);
+    }
+
+    protected void stop() {
+        driver.quit();
+    }
+
+
+    protected void clickSubmitButton(By submitButton) {
+        driver.findElement(submitButton).click();
+    }
+
+    protected void clickCheckboxes() {
+        clickSubmitButton(sexManInput);
+        clickSubmitButton(sexWomanInput);
+    }
+
+    protected void chooseDropDowns() {
+        Select dayDropdown = new Select(driver.findElement(birthdayDayDropdown));
+        dayDropdown.selectByVisibleText("10");
+        Select mounthDropdown = new Select(driver.findElement(birthdayMounthDropdown));
+        mounthDropdown.selectByIndex(2);
+        Select yearDropdown = new Select(driver.findElement(birthdayYearDropdown));
+        yearDropdown.selectByIndex(3);
+    }
+
+    protected void fillRegistrationInputs() {
+        driver.findElement(firstNameInput).sendKeys("Vasia");
+        driver.findElement(lastNameInput).sendKeys("Pupkin");
+        driver.findElement(phoneNumberInput).sendKeys("0956754433");
+        driver.findElement(passwodInput).sendKeys("somePassword");
+    }
+
+
+}
