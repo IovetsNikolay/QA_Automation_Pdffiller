89f7f08992688d270ce0393bbbec92e332790c41
cowy123 <indicow@gmail.com>
2019-04-07 21:13:22 +0300

    BankCurrency is finished

--- src/test/java/lesson3/HomeWork/BankCurrency.java
+++ src/test/java/lesson3/HomeWork/BankCurrency.java
@@ -8,6 +8,10 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class BankCurrency {
@@ -29,7 +33,6 @@ public class BankCurrency {
     private By oschadCurrencySell = By.xpath("//strong[@class='sell-USD']");
     private By nbuCurrency = By.xpath("//td[contains(text(),'Долар США')]/following-sibling::td");
 
-
     @BeforeTest
 
     private void init() {
@@ -44,10 +47,10 @@ public class BankCurrency {
 
     @Test
 
-    public void MainMethod () {
+    public void MainMethod() {
         driver.navigate().to(privatUrl);
         String privatCurrencyValue = driver.findElement(privatCurrency).getText();
-        String privatStrArr [] = privatCurrencyValue.split(" / ");
+        String privatStrArr[] = privatCurrencyValue.split(" / ");
         driver.navigate().to(ukrsibtUrl);
         String ukrsibCurrencyBuyValue = driver.findElement(ukrsibCurrencyBuy).getText();
         String ukrsibCurrencySellValue = driver.findElement(ukrsibCurrencySell).getText();
@@ -61,28 +64,101 @@ public class BankCurrency {
         String nbuCurrencyValue = driver.findElement(nbuCurrency).getText();
 
         //System.out.println("privatCurrencyValue " + privatCurrencyValue);
-        System.out.println("privat Buy / Sell: " + privatStrArr [0] + " / " + privatStrArr [1]);
+        System.out.println("privat Buy / Sell: " + privatStrArr[0] + " / " + privatStrArr[1]);
         System.out.println("ukrsib Buy / Sell: " + ukrsibCurrencyBuyValue + " / " + ukrsibCurrencySellValue);
-        System.out.println("universal Buy / Sell: " + universalCurrencyBuyValue  + " / " + universalCurrencySellValue);
-        System.out.println("oschad Buy / Sell: " + oschadCurrencyBuyValue  + " / " + oschadCurrencySellValue);
+        System.out.println("universal Buy / Sell: " + universalCurrencyBuyValue + " / " + universalCurrencySellValue);
+        System.out.println("oschad Buy / Sell: " + oschadCurrencyBuyValue + " / " + oschadCurrencySellValue);
         System.out.println("nbuCurrencyValue: " + nbuCurrencyValue);
 
-        double privatCurrencyBuyInt = Double.parseDouble(privatStrArr [0]);
-        double privatCurrencySellInt = Double.parseDouble(privatStrArr [1]);
-        double ukrsibCurrencyBuyInt = Double.parseDouble(ukrsibCurrencyBuyValue);
-        double ukrsibCurrencySellInt = Double.parseDouble(ukrsibCurrencySellValue);
-        double universalCurrencyBuyInt = Double.parseDouble(universalCurrencyBuyValue);
-        double universalCurrencySellInt = Double.parseDouble(universalCurrencySellValue);
-        double oschadCurrencyBuyInt = Double.parseDouble(oschadCurrencyBuyValue);
-        double oschadCurrencySellInt = Double.parseDouble(oschadCurrencySellValue);
-        double nbuCurrencyInt = Double.parseDouble(nbuCurrencyValue);
+        double privatCurrencyBuyDouble = Double.parseDouble(privatStrArr[0]);
+        double privatCurrencySellDouble = Double.parseDouble(privatStrArr[1]);
+        double ukrsibCurrencyBuyDouble = Double.parseDouble(ukrsibCurrencyBuyValue);
+        double ukrsibCurrencySellDouble = Double.parseDouble(ukrsibCurrencySellValue);
+        double universalCurrencyBuyDouble = Double.parseDouble(universalCurrencyBuyValue);
+        double universalCurrencySellDouble = Double.parseDouble(universalCurrencySellValue);
+        double oschadCurrencyBuyDouble = Double.parseDouble(oschadCurrencyBuyValue);
+        double oschadCurrencySellDouble = Double.parseDouble(oschadCurrencySellValue);
+        double nbuCurrencyDouble = (Double.parseDouble(nbuCurrencyValue)) / 100;
 
+        List<Double> buyValues = new ArrayList<>();
+        buyValues.add(privatCurrencyBuyDouble);
+        buyValues.add(ukrsibCurrencyBuyDouble);
+        buyValues.add(universalCurrencyBuyDouble);
+        buyValues.add(oschadCurrencyBuyDouble);
+        buyValues.add(nbuCurrencyDouble);
+
+        List<Double> sellValues = new ArrayList<>();
+        sellValues.add(privatCurrencySellDouble);
+        sellValues.add(ukrsibCurrencySellDouble);
+        sellValues.add(universalCurrencySellDouble);
+        sellValues.add(oschadCurrencySellDouble);
+        sellValues.add(nbuCurrencyDouble);
+
+        double MidBuy;
+        MidBuy = 0.0;
+        for (Double buyCurency : buyValues) MidBuy = MidBuy + buyCurency;
+        MidBuy /= buyValues.size();
+        System.out.println("Mid Buy Currency: " + MidBuy);
+
+        double MidSell;
+        MidSell = 0.0;
+        for (Double sellCurency : sellValues) MidSell = MidSell + sellCurency;
+        MidSell /= sellValues.size();
+        System.out.println("Mid Sell Currency: " + MidSell);
+
+        int minIndexBuyValues = buyValues.indexOf(Collections.min(buyValues));
+        //int maxIndexBuyValues = buyValues.indexOf(Collections.max(buyValues));
+        //int minIndexSellValues = buyValues.indexOf(Collections.min(sellValues));
+        int maxIndexSellValues = sellValues.indexOf(Collections.max(sellValues));
+
+        System.out.println("minIndexBuyValues" + minIndexBuyValues);
+        System.out.println("maxIndexSellValues" + maxIndexSellValues);
+
+        switch (minIndexBuyValues) {
+            case 0:
+                System.out.println("Minimum currency buy value is in Privat Bank");
+                break;
+            case 1:
+                System.out.println("Minimum currency buy value is in Ukrsib Bank");
+                break;
+            case 2:
+                System.out.println("Minimum currency buy value is in Universal Bank");
+                break;
+            case 3:
+                System.out.println("Minimum currency buy value is in Oschad Bank");
+                break;
+            case 4:
+                System.out.println("Minimum currency buy value is in Nac Bank");
+                break;
+            default:
+                break;
+        }
+
+        switch (maxIndexSellValues) {
+            case 0:
+                System.out.println("Maximum currency sell value is in Privat Bank");
+                break;
+            case 1:
+                System.out.println("Maximum currency sell value is in Ukrsib Bank");
+                break;
+            case 2:
+                System.out.println("Maximum currency sell value is in Universal Bank");
+                break;
+            case 3:
+                System.out.println("Maximum currency sell value is in Oschad Bank");
+                break;
+            case 4:
+                System.out.println("Maximum currency sell value is in Nac Bank");
+                break;
+            default:
+                break;
+        }
 
     }
 
     @AfterTest
 
-    public void stop () {
+    public void stop() {
         driver.quit();
     }
 
